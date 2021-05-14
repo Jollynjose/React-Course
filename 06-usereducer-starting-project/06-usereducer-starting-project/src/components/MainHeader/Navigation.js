f4dbb4d3a3d275f8115a6119e50c775ed327d756
jollynjose21 <jsainthilaire@intellisys.com.do>
2021-05-14 09:36:43 -0400

    qa

---
+++ 06-usereducer-starting-project/06-usereducer-starting-project/src/components/MainHeader/Navigation.js
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react';
+import AuthContext from '../../store/auth-content';
+import Button from '../UI/Button/Button'
+import classes from './Navigation.module.css';
+
+const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+  return(
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <Button onClick={ctx.onLogout}>Logout</Button>
+          </li>
+        )}
+      </ul>
+    </nav>
+    ) 
+};
+
+export default Navigation;
---
+++ 06-usereducer-starting-project/06-usereducer-starting-project/src/store/auth-content.js
@@ -0,0 +1,9 @@
+import React, {useState}from 'react';
+
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    onLogout: ()=>{}
+});
+
+
+export default AuthContext;
